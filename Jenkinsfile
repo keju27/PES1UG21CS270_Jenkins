22208a805b3237f2af917f112465c5967b3a3fc6
Karthik Ram <karthikram2703@gmail.com>
2024-03-09 12:34:12 +0530

    changes

--- Jenkinsfile
+++ Jenkinsfile
@@ -10,7 +10,7 @@ pipeline{
             }
         }
         stage('Test'){
-            stepss{
+            steps{
                 sh './output'
                 echo 'Test Stage Successful'
             }
--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "I have successfully built and run this main.cpp file\n";
   cout << "I am PES1UG21CS270\n";
-  cout << "I am very Happy\n";
+  cout << "I am very Happy\n"
   return 0;
 }
