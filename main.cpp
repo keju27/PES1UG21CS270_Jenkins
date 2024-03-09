f18cd67dd3f52b103e3643f40e92888c7783c737
Karthik Ram <karthikram2703@gmail.com>
2024-03-09 12:30:16 +0530

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//File: main.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "I have successfully built and run this main.cpp file\n";
+  cout << "I am PES1UG21CS270\n";
+  cout << "I am very Happy\n";
+  return 0;
+}
